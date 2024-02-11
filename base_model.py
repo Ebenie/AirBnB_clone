0b3ccf57ca6d67f0865b1f8f3c193facc7aca6e4
Ebenie <abrahamavenue163@.com>
2024-02-11 11:10:11 -0800

    updated base_mode

--- base_model.py
+++ base_model.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a BaseModel class
- that serves as a base class for other classes.
- It provides methods for initializing object instances, 
- converting objects to strings, saving objects, and
- converting objects to dictionary representations."""
+"""BaseModel module"""
 import uuid
 from datetime import datetime
-import models
+
 
 class BaseModel:
-    """This class initializes objects with unique identifiers
-    and date-time attributes, allowing for string
-    conversion and dictionary representation of
-    objects"""
+    """BaseModel class"""
     def __init__(self, *args, **kwargs):
-        """Initialization method that
-        method call during instanciation"""
+        """Initialization method"""
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -25,21 +17,17 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
-        """String representation method according to
-        the rule of the project"""
+        """String representation method"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at attribute with current datetime
-        and save it also in the object storage"""
+        """Update updated_at attribute with current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
+
     def to_dict(self):
-        """Return dictionary representation of instance
-        after copy the attributes to the variable new_dict"""
+        """Return dictionary representation of instance"""
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
