3091304f21f11771e441680ce3d8c6b101da6f8a
Ebenie <abrahamavenue163@.com>
2024-02-11 11:12:09 -0800

    updated base_mode

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""BaseModel module"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel class"""
+    def __init__(self, *args, **kwargs):
+        """Initialization method"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation method"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at attribute with current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return dictionary representation of instance"""
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
+
