ae8d02c3b1bc19116dc9c65d86d5a455bbd4a67e
Ebenie <abrahamavenue163@.com>
2024-02-12 03:46:44 -0800

    the updated code

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
-"""BaseModel module"""
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
-    """BaseModel class"""
+    """BaseModel class that defines common attributes/methods for other classes."""
+    
     def __init__(self, *args, **kwargs):
-        """Initialization method"""
+        """Initialize BaseModel."""
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -16,21 +14,22 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """String representation method"""
+        """Return string representation of BaseModel instance."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at attribute with current datetime"""
+        """Update the public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return dictionary representation of instance"""
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        """Return a dictionary containing all keys/values of __dict__ of the instance."""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
 
